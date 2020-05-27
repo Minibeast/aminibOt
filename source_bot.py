2eda6155f433c87773f8bbc39b4d99d7821c27ed
Minibeast <iancolvin1996@gmail.com>
2020-05-27 15:34:33 -0400

    source bot thread added

---
+++ source_bot.py
@@ -0,0 +1,70 @@
+import socket
+import time
+import urllib
+import urllib.request
+import urllib.error
+import datetime
+import json
+import sys
+
+HOST = "irc.twitch.tv"
+PORT = 6667
+NICK = "aminibOt"
+PASS = str(sys.argv[1])  # OAuth run through argument
+CHAN = "source28"
+
+s = socket.socket()
+s.connect((HOST, PORT))
+s.send(bytes("PASS " + PASS + "\r\n", "UTF-8"))
+s.send(bytes("NICK " + NICK + "\r\n", "UTF-8"))
+s.send(bytes("JOIN #" + CHAN + " \r\n", "UTF-8"))
+
+
+def time_format(sec):
+    return str(datetime.timedelta(seconds=int(sec)))
+
+
+def send_message(text):
+    s.send(bytes("PRIVMSG #" + CHAN + " :" + text + "\r\n", "UTF-8"))
+    time.sleep(1)
+
+
+def get_time(url):
+    try:
+        data = json.loads(urllib.request.urlopen(url).read())
+        return time_format(data["data"]["runs"][0]["run"]["times"]["primary_t"])
+    except LookupError or urllib.error.URLError:
+        return "Error"
+
+
+while True:
+    line = str(s.recv(1024))
+    if "End of /NAMES list" in line:
+        break
+
+while True:
+    for line in str(s.recv(1024)).split('\\r\\n'):
+        if "PING" in line:
+            s.send(bytes("PONG :tmi.twitch.tv\r\n", "UTF-8"))
+
+        parts = line.split(':')
+        if len(parts) < 3:
+            continue
+
+        if "QUIT" not in parts[1] and "JOIN" not in parts[1] and "PART" not in parts[1]:
+            message = parts[2][:len(parts[2])]
+        else:
+            message = ""
+
+        usernamesplit = parts[1].split("!")
+        username = usernamesplit[0]
+
+        # print(username + ": " + message)
+        if username.startswith("manofsteel"):
+            send_message("/ban " + str(username))
+
+        elif message.startswith("!kill") and username == "source28":
+            quit()
+
+        elif message.startswith("!test") and username == "source28":
+            send_message("ping pong the bot is alive")
