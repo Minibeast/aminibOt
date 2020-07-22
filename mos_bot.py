4cd68cfb685bbf9e5c47a8cbd98cb4a37b9759b8
Minibeast <minibossemail@gmail.com>
2020-07-22 16:20:06 -0400

    removed source28

--- mos_bot.py
+++ mos_bot.py
@@ -1,12 +1,11 @@
 import socket
-import time
 import sys
 
 HOST = "irc.twitch.tv"
 PORT = 6667
 NICK = "aminibOt"
 PASS = str(sys.argv[1])  # OAuth run through argument
-CHANNELS = ["source28", "sailo93", "cjya2016", "l1ghtrc"]
+CHANNELS = ["sailo93", "cjya2016", "l1ghtrc"]
 
 s = socket.socket()
 s.connect((HOST, PORT))
